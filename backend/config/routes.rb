02ba0cd5b491236a8d7985e750ed8ce45b585d92
Yuki Okuno <yuki.okuno@smarthr.co.jp>
2025-02-13 21:59:41 +0900

    devise_token_authを利用した認証の追加

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  scope :api do
+    scope format: 'json' do
+      mount_devise_token_auth_for 'User', at: 'auth'
+    end
+  end
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "health" => "health#show", as: :health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
 end
