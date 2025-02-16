6964c9929c30362322a9dc803a1f835207255e50
Okkun555 <y.okuno.btm@gmail.com>
2025-02-16 22:49:51 +0900

    specで認証が必要な場合の対応

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
   scope :api do
     scope format: 'json' do
-      mount_devise_token_auth_for 'User', at: 'auth', controllers: {
-        registrations: 'api/auth/registrations',
-      }
+      mount_devise_token_auth_for 'User', at: 'auth'
     end
   end
 
