0e252b6f4a10dee7e0dc702815a5b046f9dd4640
Okkun555 <y.okuno.btm@gmail.com>
2025-02-16 21:35:19 +0900

    rubocopを適用

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   scope :api do
     scope format: 'json' do
-      mount_devise_token_auth_for 'User', at: 'auth'
+      mount_devise_token_auth_for 'User', at: 'auth', controllers: {
+        registrations: 'api/auth/registrations',
+      }
     end
   end
 
